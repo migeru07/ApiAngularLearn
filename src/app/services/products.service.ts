0b62fa3659ef9b35eb7f2bd36b3a1a641b7313dd
migeru07 <34461583+migeru07@users.noreply.github.com>
2023-05-20 14:37:37 -0400

    pipe(retry)

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 
 import { CreateProductDTO, Product, UpdateProductDTO } from './../models/product.model';
 
@@ -8,7 +9,7 @@ import { CreateProductDTO, Product, UpdateProductDTO } from './../models/product
 })
 export class ProductsService {
 
-  private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products'
+  private apiUrl = 'https://young-sands-07814.heroku1u1app.com/api/products'
 
   constructor(
     private http: HttpClient
@@ -28,7 +29,10 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params });
+    return this.http.get<Product[]>(this.apiUrl, { params })
+    .pipe(
+      retry(3)
+    );
   }
 
   getProduct(id: string) {
