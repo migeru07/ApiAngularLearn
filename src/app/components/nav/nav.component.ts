4e9c9332aa0a5dfa72ce8ab74ed8fee7b0813d26
migeru07 <34461583+migeru07@users.noreply.github.com>
2023-06-05 11:38:31 -0400

    Finish CategoryMenu

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';;
+import { Component, OnInit } from '@angular/core';
 
-import { StoreService } from '../../services/store.service'
+import { StoreService } from '../../services/store.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 import { User } from 'src/app/models/user.model';
+import { Category } from 'src/app/models/category.model';
 
 @Component({
   selector: 'app-nav',
@@ -14,6 +16,7 @@ export class NavComponent implements OnInit {
 
   activeMenu = false;
   counter = 0;
+  categories: Category[] = [];
   profile: User = {
     id: '', 
     name: '',
@@ -23,13 +26,15 @@ export class NavComponent implements OnInit {
 
   constructor(
     private storeService: StoreService,
-    private authService: AuthService
+    private authService: AuthService,
+    private categoriesService: CategoriesService
   ) { }
 
   ngOnInit(): void {
     this.storeService.myCart$.subscribe(products => {
       this.counter = products.length;
     });
+    this.getAllCategories();
   }
 
   toggleMenu() {
@@ -40,6 +45,13 @@ export class NavComponent implements OnInit {
     this.authService.loginAndGet('juan@miguel.do','123123')
     .subscribe(user => {
       this.profile = user; 
+    })
+  }
+
+  getAllCategories() {
+    this.categoriesService.getAll()
+    .subscribe(data => {
+      this.categories = data;      
     })
   }
 
