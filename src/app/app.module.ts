f7ee950980df75e0f7111c0d25d6ce357c546ff8
migeru07 <34461583+migeru07@users.noreply.github.com>
2023-06-09 10:57:03 -0400

    AddingLazyLoading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,53 +5,17 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ImgComponent } from './website/components/img/img.component';
-import { ProductComponent } from './website/components/product/product.component';
-import { ProductsComponent } from './website/components/products/products.component';
-import { NavComponent } from './website/components/nav/nav.component';
-import { ReversePipe } from './website/pipes/reverse.pipe';
-import { TimeAgoPipe } from './website/pipes/time-ago.pipe';
-import { HighlightDirective } from './website/directives/highlight.directive';
+
 import { register } from 'swiper/element/bundle';
-import { SwiperDirective } from './website/directives/swiper.directive';
-import { StreamsSwiperComponent } from './website/components/streams-swiper/streams-swiper.component';
 import { TimeInterceptor } from './interceptors/time.interceptor';
 import { TokenInterceptor } from './interceptors/token.interceptor';
-import { HomeComponent } from './website/pages/home/home.component';
-import { NotFoundComponent } from './website/pages/not-found/not-found.component';
-import { CategoryComponent } from './website/pages/category/category.component';
-import { MycartComponent } from './website/pages/mycart/mycart.component';
-import { LoginComponent } from './website/pages/login/login.component';
-import { RegisterComponent } from './website/pages/register/register.component';
-import { RecoveryComponent } from './website/pages/recovery/recovery.component';
-import { ProfileComponent } from './website/pages/profile/profile.component';
-import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
-import { LayoutComponent } from './website/components/layout/layout.component';
 
 
 register();
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ImgComponent,
-    ProductComponent,
-    ProductsComponent,
-    NavComponent,
-    ReversePipe,
-    TimeAgoPipe,
-    HighlightDirective,
-    HomeComponent,
-    NotFoundComponent,
-    CategoryComponent,
-    MycartComponent,
-    LoginComponent,
-    RegisterComponent,
-    RecoveryComponent,
-    ProfileComponent,
-    ProductDetailComponent,
-    LayoutComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
